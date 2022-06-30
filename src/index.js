505172c69853a624b7e66c0fb6f6bcb28d931660
tabku <41511147+tabku@users.noreply.github.com>
2022-07-01 08:45:13 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const App = () => {
+  return (
+    <>
+      <h1>hoge</h1>
+      <h2>hoge</h2>
+    </>
+  );
+};
+
+ReactDom.render(<App />, document.getElementById("root"));
