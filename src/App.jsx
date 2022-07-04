db8b6dd2ba413518f71143ee8f9cd7be53a85d7c
tabku <41511147+tabku@users.noreply.github.com>
2022-07-04 23:32:13 +0900

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
-import React from "react";
+/* eslint react-hooks/exhaustive-deps: off */
+import React, { useEffect, useState } from "react";
 import ColorfulMessage from "./components/colorfulMessage";
 
 const App = () => {
-  const onClickBtn = () => {
-    alert();
+  const [num, setNum] = useState(0);
+  const [faceShowFlg, setFaceShowFlg] = useState(false);
+
+  const onClickCountUp = () => {
+    setNum(num + 1);
   };
+
+  const onClickSwitchShowFlag = () => {
+    setFaceShowFlg(!faceShowFlg);
+  };
+
+  useEffect(() => {
+    if (num > 0) {
+      if (num % 3 === 0) {
+        faceShowFlg || setFaceShowFlg(true);
+      } else {
+        faceShowFlg && setFaceShowFlg(false);
+      }
+    }
+  }, [num]);
 
   return (
     <>
       <ColorfulMessage color="blue" message="hello" />
       <ColorfulMessage color="green">hello hoge</ColorfulMessage>
-
-      <button onClick={onClickBtn}>btn</button>
+      <button onClick={onClickCountUp}>カウントアップ!</button>
+      <p>{num}</p>
+      <button onClick={onClickSwitchShowFlag}>on off</button>
+      {faceShowFlg && <p>( ´∀｀)</p>}
     </>
   );
 };
